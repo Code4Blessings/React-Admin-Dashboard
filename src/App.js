24a259366948e861a8c114351852f53b794889c8
Code4Blessings <robincw35@gmail.com>
2024-01-10 13:36:54 -0500

    Worked on TopBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+ import React from 'react';
+ import { ColorModeContext, useMode } from './theme';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import TopBar from './scenes/global/TopBar';
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app">
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <TopBar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
